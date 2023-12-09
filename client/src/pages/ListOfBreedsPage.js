94952a3f0f5628bbc79ad636d05724345a79fd88
nlg-790 <nlg790@gmail.com>
2023-12-09 10:59:04 -0500

    List of Breeds Background
    Added a background to the list of breeds page

--- client/src/pages/ListOfBreedsPage.js
+++ client/src/pages/ListOfBreedsPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './ListOfBreedsPage.css';
 
 const ListOfBreedsPage = () => {
   const [breeds, setBreeds] = useState([]);
@@ -16,19 +17,19 @@ const ListOfBreedsPage = () => {
   }, []);
 
   return (
-    <div>
+    <div className='list-of-breeds-page'>
       <h1>Breed List!</h1>
-      <p>Here's a Large List of Various Feline Breeds!</p>
-      <button><Link to="/">Back to Home</Link></button> 
-      {breeds.map(breed => (
-        <div key={breed.id}>
-          <h2>{breed.name}</h2>
-          <p>{breed.description}</p>
-        </div>
-      ))}
-      <button><Link to="/">Back to Home</Link></button> 
-    </div>
-  );
+        <p>Here's a Large List of Various Feline Breeds!</p>
+        <button><Link to="/">Back to Home</Link></button> 
+        {breeds.map(breed => (
+          <div key={breed.id}>
+            <h2>{breed.name}</h2>
+            <p>{breed.description}</p>
+          </div>
+        ))}
+        <button><Link to="/">Back to Home</Link></button> 
+      </div>
+    );
 };
 
 export default ListOfBreedsPage;
