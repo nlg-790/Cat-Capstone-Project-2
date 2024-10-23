b39c50ac033ebd8a8fefaad062f9a9c0a97e62c9
nlg-790 <nlg790@gmail.com>
2024-10-23 08:54:11 -0400

    server.js
    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
+const path = require('path'); 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
